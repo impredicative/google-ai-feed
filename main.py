55e4144dd353988b0de319ce094c4989445de821
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-06-28 21:05:47 -0400

    Add main module

---
+++ main.py
@@ -0,0 +1,17 @@
+import logging
+from typing import Dict, Tuple
+
+import flask
+
+from feed.feed import Feed
+
+log = logging.getLogger(__name__)
+feed = Feed()
+
+
+def serve(request: flask.Request) -> Tuple[bytes, int, Dict[str, str]]:
+    hget = request.headers.get
+    log.info('Received request from %s from %s, %s, %s.', hget('X-Appengine-User-Ip'),
+             hget('X-Appengine-City'), hget('X-Appengine-Region'), hget('X-Appengine-Country'))
+    output = feed.feed()
+    return output, 200, {'Content-Type': 'text/xml; charset=utf-8'}
