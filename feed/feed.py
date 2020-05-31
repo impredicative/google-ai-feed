29f2da206e7e0be4de47182b8995d18728882238
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-05-31 14:16:01 -0400

    Move filtering inside loop

--- feed/feed.py
+++ feed/feed.py
@@ -22,7 +22,9 @@ def filename_to_id(filename: str) -> int:
 
 
 class Feed:
-    def __init__(self):
+    def __init__(self) -> None:
+        log.info('The %s whitelisted research areas are: %s',
+                 len(config.RESEARCH_AREAS), ', '.join(config.RESEARCH_AREAS))
         self._areas = {f'research-area-{a.lower().replace(" ", "-")}' for a in config.RESEARCH_AREAS}
 
     @staticmethod
@@ -38,22 +40,29 @@ class Feed:
         log.debug('Reading %s', config.REQUEST_URL)
         pubs = requests.get(config.REQUEST_URL, timeout=config.REQUEST_TIMEOUT).json()['publications']
         log.info('Response for request URL has %s entries.', f'{len(pubs):n}')
-        log.info('The %s whitelisted research areas are: %s',
-                 len(config.RESEARCH_AREAS), ', '.join(config.RESEARCH_AREAS))
-        pubs = [pub for pub in pubs if not set(pub['tag_pks']).isdisjoint(self._areas)]  # Remove null intersections.
-        log.info('%s of these entries exist for the whitelisted research areas.', len(pubs))
-        pubs = [p for p in pubs if ('URL\t' in p['bibtex'])]  # Remove entries without a download link.
-        log.info('%s of these entries have a download link.', len(pubs))
+
+        # Sort by ID
+        pubs = [pub for pub in pubs if pub['filename_html']]
         for pub in pubs:
             pub[':id'] = filename_to_id(pub['filename_html'])
         pubs.sort(key=lambda pub: pub[':id'], reverse=True)
-        pubs = pubs[:config.MAX_ENTRIES]
 
+        # Create feed
         feed = self._init_feed()
-        for idx, pub in enumerate(pubs):
+        num_added = 0
+        for pub in pubs:
+            if num_added > config.MAX_ENTRIES:
+                break
+
+            is_research_area_whitelisted = not set(pub['tag_pks']).isdisjoint(self._areas)
+            if not is_research_area_whitelisted:
+                continue
+
+            has_download_link = 'URL\t' in pub['bibtex']
+            if not has_download_link:
+                continue
+
             title = pub['title']
-            desc = textwrap.shorten(pub['description_html'], float('inf'))  # type: ignore
-            desc = ''.join(c for c in desc if c.isprintable())
             url = config.PUB_URL_FORMAT.format(pub_id=pub[':id'])
             guid = str(pub[':id'])
             categories = pub['tag_pks']  # Often starts with "research-area-" or "team-".
@@ -62,12 +71,16 @@ class Feed:
             entry.title(title)
             entry.link(href=url)
             entry.guid(guid, permalink=False)
-            entry.description(desc)
+            if 'abstract' in pub:
+                desc = textwrap.shorten(pub['abstract'], float('inf'))  # type: ignore
+                desc = ''.join(c for c in desc if c.isprintable())
+                entry.description(desc)
             for category in categories:
                 entry.category(term=category)
 
             log.debug('Added pub #%s having ID %s, title %s (%s), and categories: %s',
-                      idx + 1, pub[':id'], repr(title), pub['year'], ', '.join(c for c in categories))
+                      num_added + 1, pub[':id'], repr(title), pub['year'], ', '.join(c for c in categories))
+            num_added += 1
 
         text: bytes = feed.rss_str(pretty=True)
         log.info('Output feed has %s items and size %s.', len(pubs), humanize_len(text))
